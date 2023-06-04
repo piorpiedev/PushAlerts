59f6fabf7fef8bcf8aa9085f2281fd3f3fd33599
piorpiedev <contact@piorpie.com>
2023-06-04 17:44:12 +0200

    Added local config

--- conf.py
+++ conf.py
@@ -1,4 +1,7 @@
 import os
 
-BOT_TOKEN = os.environ["botToken"]
-BASE_URL = os.environ["baseUrl"]
+if os.path.exists("lConf.py"):
+    from lConf import BOT_TOKEN, BASE_URL
+else:
+    BOT_TOKEN = os.environ["BOT_TOKEN"]
+    BASE_URL = os.environ["BASE_URL"]
