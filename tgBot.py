d6232902ea55437be43ce360281cf0551dbdd690
piorpiedev <contact@piorpie.com>
2023-06-04 18:06:23 +0200

    Fixed typo

--- tgBot.py
+++ tgBot.py
@@ -14,7 +14,7 @@ def isSubbed(update: Update):
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await reply("Hello there! " + ("Scrivi /sub per iscriverti" if isSubbed(update) else "Sei già iscritto, scrivi /unsub se vuoi disiscriverti"), update, context)
+    await reply("Hello there! " + ("Sei già iscritto, scrivi /unsub se vuoi disiscriverti" if isSubbed(update) else "Scrivi /sub per iscriverti"), update, context)
 
 
 async def sub(update: Update, context: ContextTypes.DEFAULT_TYPE):
