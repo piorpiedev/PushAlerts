78becb927046dc19819e92fdfc2b197ba51091d7
piorpiedev <contact@piorpie.com>
2023-09-28 18:40:36 +0200

    Automatically subbed the user on first msg

--- tgBot.py
+++ tgBot.py
@@ -14,8 +14,16 @@ def isSubbed(update: Update):
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await reply("Hello there! " + ("Sei già iscritto, scrivi /unsub se vuoi disiscriverti" if isSubbed(update) else "Scrivi /sub per iscriverti") + 
+    print(update.message.id)
+    firstMsg = update.message.id == 1
+    if firstMsg: commons.subs.sub(getUserId(update))
+
+    await reply("Hello there! " + ("Essendo questa la tua prima conversazione col bot, sei stato iscritto automaticamente!" if firstMsg else 
+        ("Sei già iscritto, scrivi /unsub se vuoi disiscriverti" if isSubbed(update) else "Scrivi /sub per iscriverti")) + 
         "\n\n" + str(len(commons.subs.subs)) + " persone si sono già iscritte! 😃", update, context)
+    
+    if firstMsg:
+        await reply("Ora che sei iscritto, riceverai una notifica ogni volta che una circolare verrà pubblicata\nScrivi /unsub per disiscriverti", update, context)
 
 
 async def sub(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -33,4 +41,4 @@ async def unsub(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await reply("Non sei già iscritto! Scrivi /sub per iscriverti e venire avvisato ogni volta che esce una circolare", update, context)
 
 async def notFound(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await reply("Mi spiace ma conosco solo i comandi /sub, /unsub e /last :(", update, context)
+    await reply("Mi spiace ma conosco solo i comandi /start, /sub e /unsub :(", update, context)
